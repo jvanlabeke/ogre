24bbddc8c62a5348b7fbee3ee6d738368949baf0
Pavel Rojtberg <rojtberg@gmail.com>
2019-05-01 16:25:16 +0200

    1.12 api docs

---
+++ api/1.12/navtreeindex147.js
@@ -0,0 +1,10 @@
+var NAVTREEINDEX147 =
+{
+"volume.html":[2,7],
+"volume.html#creation":[2,7,1],
+"volume.html#editing":[2,7,4],
+"volume.html#howto":[2,7,0],
+"volume.html#intersecting":[2,7,3],
+"volume.html#triangles":[2,7,2],
+"working-with-numpy.html":[2,5]
+};
